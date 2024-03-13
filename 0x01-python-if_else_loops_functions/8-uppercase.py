c92e641949cf80e10a8a6cc49ef7aa332372be2c
Jikrou <jamaleddine.zait@gmail.com>
2024-03-13 01:06:28 +0000

    upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(97, 123):
+            i = chr(ord(i) - 32)
+            print("{}".format(i), end="")
+        else:
+            print(i, end="")
+    print(" ")
