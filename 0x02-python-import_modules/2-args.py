7a41dfb42fbd6fc87382460e5495d58e51b75b5a
Jikrou <jamaleddine.zait@gmail.com>
2024-03-14 23:22:19 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arguments = len(argv) - 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print("{} argument:".format(arguments))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(arguments))
+        for i in range(1, arguments + 1):
+            print("{}: {}".format(i, argv[i]))
