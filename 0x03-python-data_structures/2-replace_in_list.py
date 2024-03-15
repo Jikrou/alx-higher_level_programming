f7010b5d72ea1dfb0ca6e1815f84ca183dcc7877
Jikrou <jamaleddine.zait@gmail.com>
2024-03-15 23:27:11 +0000

    task3

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in range(len(my_list)):
+        if idx >= 0:
+            if i == idx:
+                my_list[i] = element
+                return my_list
