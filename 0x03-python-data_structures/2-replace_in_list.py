cad25f7823392cddb88bfe59863806dc9d3c664f
Jikrou <jamaleddine.zait@gmail.com>
2024-03-16 20:32:30 +0000

    replace at index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
-        if idx >= 0:
+        if len(my_list) >= idx >= 0:
             if i == idx:
                 my_list[i] = element
                 return my_list
+        elif idx < 0:
+            return my_list
+        else:
+            return my_list
