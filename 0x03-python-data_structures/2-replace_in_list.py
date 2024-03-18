8bf750887a0ef24722427d7501af2c64e778e673
Jikrou <jamaleddine.zait@gmail.com>
2024-03-18 06:13:59 +0000

    replace at indx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
         if len(my_list) >= idx >= 0:
             if i == idx:
-                my_list[i] = element
+                my_list[idx] = element
                 return my_list
         elif idx < 0:
             return my_list
