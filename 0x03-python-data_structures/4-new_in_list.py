4ce6d73f7010c076e6520c7fb3ba88150b11d651
Jikrou <jamaleddine.zait@gmail.com>
2024-03-18 06:52:56 +0000

    new element in idx newlist

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,12 +2,10 @@
 def new_in_list(my_list, idx, element):
     cplist = my_list[:]
     len_list = len(my_list) - 1
-    for i in range(len_list):
-        if len_list >= idx >= 0:
-            if i == idx:
-                cplist[idx] = element
-                return cplist
-        elif idx < 0:
-            return cplist
-        else:
-            return cplist
+    if len_list >= idx >= 0:
+        cplist[idx] = element
+        return cplist
+    elif idx < 0:
+        return cplist
+    else:
+        return cplist
