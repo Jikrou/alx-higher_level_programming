7baf275adfb654baba6bcd4af91a4639897c75fd
Jikrou <jamaleddine.zait@gmail.com>
2024-04-02 21:42:09 +0000

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        out = 0
+        for i in my_list:
+            print("{}".format(i), end="")
+            out += 1
+            if out >= x:
+                break
+        print()
+        return out
+    except TypeError:
+        pass
