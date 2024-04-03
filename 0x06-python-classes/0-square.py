99967c490af36c6683994801be3a91ee30c51274
Jikrou <jamaleddine.zait@gmail.com>
2024-04-03 22:30:59 +0000

    docstring module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+""" 0-square
+
+This module contains a simple class.
+
+    Classes:
+        Square: a class that define a square.
+
+"""
+
+
 class Square:
+
     """ A class that define an empty square. """
     pass
