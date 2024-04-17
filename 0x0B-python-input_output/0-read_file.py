03b3eb6e532a72ae7aa59a9e5bd4924895464562
Jikrou <jamaleddine.zait@gmail.com>
2024-04-17 15:08:19 +0000

    newline

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """A function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r") as file:
-        content = file.read().rstrip()
+        content = file.read()
         print(content)
