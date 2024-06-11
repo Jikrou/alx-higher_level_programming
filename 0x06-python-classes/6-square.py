ff43788d1e5f0279163ad236d9dab726a9d41898
Jikrou <jamaleddine.zait@gmail.com>
2024-06-11 12:08:52 +0000

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,9 +17,10 @@ class Square:
         Args:
             __size: private instance attribute representing the size of
             the square must be a non negative int.
+            position (int, int): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -56,21 +57,21 @@ class Square:
     def position(self, value):
         """ Set the position of the square. """
 
-        if not isinstance(value, tuple) or len(value) != 2 or \
-            not all(isinstance(i, int) for i in value) or \
-                not all(i >= 0 for i in value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-
         self.__position = value
 
     def my_print(self):
-        """ Print the square with the character #."""
-
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
