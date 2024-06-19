0a1e282ada9bc1147a28d0d048f0287d2f8ce036
Jikrou <jamaleddine.zait@gmail.com>
2024-06-19 23:31:58 +0000

    Square of C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
-
 const SquAre = require('./5-square');
 
 class Square extends SquAre {
   charPrint (c) {
     if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let prinTc = [];
-        for (let j = 0; j < this.width; j++) {
-          prinTc += 'c';
-        }
-        console.log(prinTc);
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let prinTc = [];
+      for (let j = 0; j < this.width; j++) {
+        prinTc += c;
       }
+      console.log(prinTc);
     }
   }
 }
