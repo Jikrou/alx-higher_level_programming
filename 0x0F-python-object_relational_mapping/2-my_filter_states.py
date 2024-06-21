e716cee3ea79f4996e50a69cfa13ba7186608477
Jikrou <jamaleddine.zait@gmail.com>
2024-06-21 13:34:30 +0000

    Execute using format

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a script that takes an argument and displays all
+    the values in the states table. """
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    rows = cur.fetchall()
+
+    for r in rows:
+        print(r)
+
+    cur.close()
+    db.close()
