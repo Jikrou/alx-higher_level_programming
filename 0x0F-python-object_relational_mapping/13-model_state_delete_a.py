629e4296532c8e0f785beec2066bf2f7966b01f2
Jikrou <jamaleddine.zait@gmail.com>
2024-06-23 13:11:06 +0000

    update delete

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
         'mysql://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
                                             sys.argv[3]), pool_pre_ping=True)
     session = Session(engine)
-    sn = session.query(State).filter(State.name.like('%a%'))
-    for d in sn:
+    for d in session.query(State).filter(State.name.like('%a%')).all():
         session.delete(d)
     session.commit()
