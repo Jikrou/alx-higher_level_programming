88f56cc13ddc061447b7dafefc0436bc5d956d30
Jikrou <jamaleddine.zait@gmail.com>
2024-07-05 15:02:43 +0000

    POST mail

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
