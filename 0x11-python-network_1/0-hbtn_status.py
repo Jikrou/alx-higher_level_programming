7e01e38aa2952b7ba6d10537765a7adf30eefd61
Jikrou <121866583+Jikrou@users.noreply.github.com>
2024-07-05 13:57:54 +0100

    tsk0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,8 @@ Script that fetches https://intranet.hbtn.io/status
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         body = res.read()
-
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
