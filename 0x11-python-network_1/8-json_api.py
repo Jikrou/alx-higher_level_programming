eb2e1151eb3e55a996903a34ab479161f89e05c4
Jikrou <jamaleddine.zait@gmail.com>
2024-07-06 20:54:29 +0000

    Api search

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,22 +3,24 @@
 a python script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter
     """
+import requests
+import sys
+
 
 if __name__ == '__main__':
-    import requests
-    import sys
 
-    if len(sys.argv[1]) > 1:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
 
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
     try:
-        r = r.json()
-        if r == {}:
+        r_json = r.json()
+        if r_json == {}:
             print("No result")
         else:
-            print("[{}] {}".format(r.get('id'), r.get('name')))
+            print("[{}] {}".format(r_json.get('id'), r_json.get('name')))
     except ValueError:
         print("Not a valid JSON")
