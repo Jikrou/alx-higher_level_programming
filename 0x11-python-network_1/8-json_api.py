4cbbec918681085cd987e281c3dcc934ad0efe7e
Jikrou <jamaleddine.zait@gmail.com>
2024-07-06 20:37:41 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+    """
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    if len(sys.argv[1]) > 0:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data=('q', q))
+    try:
+        r = r.json()
+        if r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r('id'), r('name')))
+    except ValueError:
+        print("Not a valid JSON")
