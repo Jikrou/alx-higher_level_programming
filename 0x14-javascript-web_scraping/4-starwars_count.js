607a24f6f8fd804db93acf672f3e5822b7407026
Jikrou <jamaleddine.zait@gmail.com>
2024-07-31 19:17:40 +0000

    id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(urlBase, function (err, response, body) {
   if (err) throw err;
   const films = JSON.parse(body).results;
   const count = films.reduce((acc, film) => {
-    return acc + (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') ? 1 : 0);
+    return acc + (film.characters.some(charUrl => charUrl.endsWith('/people/18/')) ? 1 : 0);
   }, 0);
   console.log(count);
 });
