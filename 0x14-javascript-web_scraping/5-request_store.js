290f1512b7d87ff1549a3c715b9da1104a17938e
Jikrou <jamaleddine.zait@gmail.com>
2024-07-31 20:05:12 +0000

    let s write

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -10,10 +10,10 @@ const request = require('request');
 const url = process.argv[2];
 const bdText = process.argv[3];
 
-request.get(url, function (err, body) {
+request.get(url, function (err, response, body) {
   if (err) throw err;
-  const d = body.body;
-  fs.writeFile(bdText, d, (err) => {
+  const d = body;
+  fs.writeFile(bdText, d, 'utf8', (err) => {
     if (err) throw err;
   });
 });
